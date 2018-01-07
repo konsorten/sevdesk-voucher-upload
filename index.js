feeb255cf916ac247283c14051e3216eb9316973
Felix Kollmann <felix.kollmann@twinpoint.de>
2018-01-07 22:19:08 +0100

    Resolving of contacts is now done 'manually'

--- index.js
+++ index.js
@@ -110,46 +110,60 @@ class SevdeskVoucherImporter {
 
         this.debug(`Successfully Extracted information: ${Object.keys(extractions).map(k => `${k}="${extractions[k].value}"`).join(", ")}`);
 
+        // load all contacts for extended resolution
+        await this.loadAllContacts();
+
         // determine issuer contact
         // (highest priority is checked first)
         this.issuerContacts = [];
 
+        // try to find the exact name
         if (extractions.CREDITORNAME) {
-            // try to find the exact name
-            await this.findContactByName(extractions.CREDITORNAME.value);
+            await this.findContactByName(extractions.CREDITORNAME.value);        
+        } else {
+            this.debug("Skipping resolving issuer contact by exact name; name is not known");
+        }
 
-            // try to find by first word
+        // try to find the bank account
+        if (extractions.IBAN) {
+            await this.findContactByBankAccount(extractions.IBAN.value);        
+        } else {
+            this.debug("Skipping resolving issuer contact by bank account; IBAN is not known");
+        }
+
+        // try to find by first word
+        if (extractions.CREDITORNAME) {
             let nameParts = extractions.CREDITORNAME.value.split(' ', 2);
 
             if (nameParts.length >= 2) // only do this if there are at least two words
                 await this.findContactByName(nameParts[0]);
         
         } else {
-            this.debug("Skipping resolving issuer contact by name; name is not known");
+            this.debug("Skipping resolving issuer contact by first word of name; name is not known");
         }
 
         // save the voucher
         this.debug("Saving voucher...");
 
         let formData = {
-            'voucher[voucherDate]': (extractions.INVOICEDATE ? extractions.INVOICEDATE.value : moment().format('YYYY-MM-DD')),
-            'voucher[description]': (extractions.INVOICENUMBER ? extractions.INVOICENUMBER.value : ""),
+            'voucher[voucherDate]': String(extractions.INVOICEDATE ? extractions.INVOICEDATE.value : moment().format('YYYY-MM-DD')),
+            'voucher[description]': String(extractions.INVOICENUMBER ? extractions.INVOICENUMBER.value : null),
             'voucher[resultDisdar]': JSON.stringify(resultDisdar),
             'voucher[status]': '50' /* draft */,
             'voucher[taxType]': 'default',
             'voucher[creditDebit]': 'C',
             'voucher[voucherType]': 'VOU',
-            'voucher[iban]:': (extractions.IBAN ? extractions.IBAN.value : "null"),
+            'voucher[iban]:': String(extractions.IBAN ? extractions.IBAN.value : null),
             'voucher[tip]': '0',
             'voucher[mileageRate]': '0',
             'voucher[selectedForPaymentFile]': '0',
             'voucher[objectName]': 'Voucher',
             'voucher[mapAll]': 'true',
-            'voucherPosSave[0][taxRate]': (extractions.TAXRATE ? extractions.TAXRATE.value : 19).toString(),
-            'voucherPosSave[0][sum]': (extractions.NETAMOUNT ? Number.parseInt(extractions.NETAMOUNT.value) / 100 : 0).toString(),
+            'voucherPosSave[0][taxRate]': String(extractions.TAXRATE ? extractions.TAXRATE.value : 19),
+            'voucherPosSave[0][sum]': String(extractions.NETAMOUNT ? Number.parseInt(extractions.NETAMOUNT.value) / 100 : 0),
             'voucherPosSave[0][objectName]': 'VoucherPos',
             'voucherPosSave[0][mapAll]': 'true',
-            'filename': remoteFilename,
+            'filename': String(remoteFilename),
             'existenceCheck': 'true',
         };
 
@@ -157,16 +171,17 @@ class SevdeskVoucherImporter {
         if (this.issuerContacts.length > 0) {
             formData = { 
                 ...formData, 
-                'voucher[supplier][id]': this.issuerContacts[0].id,
+                'voucher[supplier][id]': String(this.issuerContacts[0].id),
                 'voucher[supplier][objectName]': 'Contact',
+                'voucher[supplierMethod]': String(this.issuerContacts[0].method),
             };
         } else {
             formData = { 
                 ...formData, 
-                'voucher[supplierName]': (extractions.CREDITORNAME ? extractions.CREDITORNAME.value : "???"),
+                'voucher[supplierName]': String(extractions.CREDITORNAME ? extractions.CREDITORNAME.value : "???"),
             };
         }
-request.debug = true;
+
         // perform save
         res = await request({
             method: 'POST',
@@ -191,39 +206,115 @@ request.debug = true;
         this.debug(`Successfully saved voucher: ${res.objects.document.id}`);
     }
 
+    async findContactByBankAccount(bankAccount) {
+        // prepare
+        bankAccount = bankAccount.replace(/[\t -]/g, '');
+
+        this.debug(`Resolving issuer contact by bank account: ${bankAccount} ...`);
+
+        // look for exact match
+        let found = this.allContacts.filter(o => o.bankAccount && o.bankAccount.replace(/[\t -]/g, '').match(new RegExp(`^${bankAccount}$`, 'i')));
+
+        if (found.length <= 0) {
+            this.debug(`No issuer contact was found by bank account: ${bankAccount}`);
+        } else {
+            for (let c of found) {
+                this.debug(`Found issuer contact by bank account: ${c.name} (#${c.id})`);
+
+                this.issuerContacts.push({ ...c, method: "bankAccountExact" });
+            }
+        }
+    }
+
     async findContactByName(name) {
         this.debug(`Resolving issuer contact by name: ${name} ...`);
 
-        let res = await request({
-            method: 'GET',
-            uri: `${this.baseUrl}/Contact`,
-            qs: {
-                cft: this.cft,
-                token: this.apiToken,
-                depth: true,
-                name: name,
-            },
-            headers: {
-                'Accept': 'application/json',
-                'Pragma': 'no-cache',
-                'Cache-Control': 'no-cache',
-            },
-            json: true,
-            gzip: true,
-        });
+        // look for exact name match
+        let found = this.allContacts.filter(o => o.name && o.name.match(new RegExp(`^${name}$`, 'i')));
 
-        if (!Array.isArray(res.objects))
-            throw new Error(`Failed to get contacts from response: ${JSON.stringify(res)}`);
-
-        if (res.objects.length <= 0) {
-            this.debug(`No issuer contact was found by name: ${name}`);
+        if (found.length <= 0) {
+            this.debug(`No issuer contact was found by exact name: ${name}`);
         } else {
-            for (let c of res.objects) {
-                this.debug(`Found issuer contact by name: ${c.name} (#${c.id})`);
+            for (let c of found) {
+                this.debug(`Found issuer contact by exact name: ${c.name} (#${c.id})`);
 
-                this.issuerContacts.push(c);
+                this.issuerContacts.push({ ...c, method: "nameExact" });
             }
         }
+
+        // look for partial name match
+        found = this.allContacts.filter(o => o.name && o.name.match(new RegExp(`${name}`, 'i')));
+
+        if (found.length <= 0) {
+            this.debug(`No issuer contact was found by partial name: ${name}`);
+        } else {
+            for (let c of found) {
+                this.debug(`Found issuer contact by partial name: ${c.name} (#${c.id})`);
+
+                this.issuerContacts.push({ ...c, method: "namePartial" });
+            }
+        }
+
+        // look for partial name2 match
+        found = this.allContacts.filter(o => o.name2 && o.name2.match(new RegExp(`${name}`, 'i')));
+
+        if (found.length <= 0) {
+            this.debug(`No issuer contact was found by partial name2: ${name}`);
+        } else {
+            for (let c of found) {
+                this.debug(`Found issuer contact by partial name2: ${c.name} (#${c.id})`);
+
+                this.issuerContacts.push({ ...c, method: "name2Partial" });
+            }
+        }
+    }
+
+    async loadAllContacts() {
+        // already loaded?
+        if (Array.isArray(this.allContacts))
+            return;
+
+        this.debug(`Loading all contacts...`);
+
+        this.allContacts = [];
+
+        let res = null;
+        let offset = 0;
+
+        do {
+            res = await request({
+                method: 'GET',
+                uri: `${this.baseUrl}/Contact`,
+                qs: {
+                    cft: this.cft,
+                    token: this.apiToken,
+                    depth: true,
+                    limit: 100,
+                    offset: offset,
+                    'orderBy[0][field]': 'create',  // 'id' field does not work
+                    'orderBy[0][arrangement]': 'asc',
+                },
+                headers: {
+                    'Accept': 'application/json',
+                    'Pragma': 'no-cache',
+                    'Cache-Control': 'no-cache',
+                },
+                json: true,
+                gzip: true,
+            });
+    
+            if (!Array.isArray(res.objects))
+                throw new Error(`Failed to get contacts from response: ${JSON.stringify(res)}`);
+
+            offset += res.objects.length;
+    
+            this.allContacts = this.allContacts.concat(res.objects);
+    
+        } while (res.objects.length > 0);
+    
+        this.debug(`Successfully loaded ${this.allContacts.length} contacts`);
+
+        //this.debug(JSON.stringify(this.allContacts[0]));
     }
 
     /**
@@ -236,6 +327,7 @@ request.debug = true;
         this.apiToken = apiToken;
         this.baseUrl = "https://my.sevdesk.de/api/v1";
         this.locked = false;
+        this.allContacts = null;
     }
 }
 
